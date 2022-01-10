3fd8531145803161387458e3e40efb553dee72ad
melnik <melnik@bifit.com>
2022-01-10 16:27:07 +0300

    Верстка мобильной версии блока example

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const menuBurger = document.querySelector('.header__toggle');
   menuBurger.addEventListener('click', burgerClickHandler);
+
+  const exampleLabelBefore = document.querySelector('.example__toggle-label--before');
+  const exampleLabelAfter = document.querySelector('.example__toggle-label--after');
+  const exampleImgBefore = document.querySelector('.example__photo-item--before');
+  const exampleImgAfter = document.querySelector('.example__photo-item--after');
+  const exampleToggler = document.querySelector('.example__toggler')
+
+  exampleLabelBefore.addEventListener('click', () => {
+    if (!exampleImgAfter.classList.contains('hidden')) {
+      exampleImgAfter.classList.toggle('hidden');
+      exampleImgBefore.classList.toggle('hidden');
+
+      exampleToggler.classList.remove('example__toggler--after');
+      exampleToggler.classList.add('example__toggler--before');
+    }
+  })
+
+  exampleLabelAfter.addEventListener('click', () => {
+    if (!exampleImgBefore.classList.contains('hidden')) {
+      exampleImgBefore.classList.toggle('hidden');
+      exampleImgAfter.classList.toggle('hidden');
+
+      exampleToggler.classList.remove('example__toggler--before');
+      exampleToggler.classList.add('example__toggler--after');
+    }
+  })
 });
 
 function burgerClickHandler(e) {
