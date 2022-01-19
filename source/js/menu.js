870dfbe86964bd1581c9c2ac1c3173705a9992d5
melnik <melnik@bifit.com>
2022-01-19 18:57:57 +0300

    Добавлены стили навигации при переходе

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //TODO разобрать с цветом на разный страницах
+  const routeLinks = document.querySelectorAll('.header__nav-link');
+  Array.from(routeLinks).forEach(link => {
+    const linkUrl = new URL(link.href);
+    console.log(linkUrl);
+    if (linkUrl.pathname === document.location.pathname && linkUrl.hash === document.location.hash) {
+      console.log('HERE');
+      // console.log(link.closest('.header__nav-item'));
+      link.closest('.header__nav-item').classList.add('header__nav-item--active');
+    }
+  })
+
   document.querySelector('.header').classList.remove('header--nojs');
 
   const menuBurger = document.querySelector('.header__toggle');
